e351b697aab159bb1916ecd3b6c59b9df3c701d7
Abhijit Pradhan (macbook) <abhijit.pradhan6@gmail.com>
2014-10-02 18:35:10 -0500

    updated p2 title link address

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 	
 		<div class="row">
 			<div class="col-sm-1 col-sm-offset-1 col-xs-2 col-xs-offset-1 back"><a href="http://p1.apradhan.me"><span class="glyphicon glyphicon-chevron-left"></span> p1</a></div>
-			<div class="col-sm-8 col-xs-12"><h1 class="title"><a href="/p2">Project 2</a></h1></div>
+			<div class="col-sm-8 col-xs-12"><h1 class="title"><a href="index.php">Project 2</a></h1></div>
 		</div>
 		<div class="row">
 			<h2 class="subtitle">xkcd Password Generator</h2>
